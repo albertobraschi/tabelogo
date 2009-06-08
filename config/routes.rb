a06a358a38052e2c89f10531e34e40548ab3f2cc
Hiroyuki Kondo <hombredeestemundo@gmail.com>
2009-06-08 22:54:18 +0900

    users, requests, shops added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :shops
+
+  map.resources :requests
+
+  map.resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -31,7 +37,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "requests"
 
   # See how all your routes lay out with "rake routes"
 
@@ -40,5 +46,5 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing the them or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
-  # map.root :controller => "home"
+
 end
